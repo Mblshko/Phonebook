aa0d177709e6188053d4615fd60f296f4116b4c0
Mblshko <mishka-007@yandex.ru>
2023-08-21 22:51:20 +0300

    fix

--- file_menegement.py
+++ file_menegement.py
@@ -7,7 +7,7 @@ class File:
     @staticmethod
     def file_create():
         '''Создаёт файл и добавляет первую строчку название колонок'''
-        with open("telephone_book.csv", "w", encoding="utf-8") as book:
+        with open("telephone_book.csv", "w", encoding="utf-8", newline='') as book:
             book.write(f"ID,Фамилия,Имя,Отчество,Название организации,"
                        f"Рабочий телефон,Личный телефон\n")
 
@@ -24,13 +24,13 @@ class File:
     @staticmethod
     def file_append(data):
         '''Добавляет в конец файла новую запись'''
-        with open("telephone_book.csv", "a", encoding="utf-8") as book:
+        with open("telephone_book.csv", "a", encoding="utf-8", newline='') as book:
             writer = csv.writer(book)
             writer.writerow(data)
 
     @staticmethod
     def file_write(data):
         '''Перезаписывает весь файл. Используется для удаления и редактирования'''
-        with open("telephone_book.csv", "w", encoding="utf-8") as book:
+        with open("telephone_book.csv", "w", encoding="utf-8", newline='') as book:
             writer = csv.writer(book)
             writer.writerows(data)
